9ecb285b96c8d2c5189a3a821a3bbf155e42139b
Jakub Badowski <badowski@inig.pl>
2016-11-17 12:42:26 +0100

    resizable

--- services/compLang.php
+++ services/compLang.php
@@ -1,6 +1,8 @@
 <?php
 
-$data = ['puppa', 'duppa', 'ryppka', 'pippka'];
+// $data = ['puppa', 'duppa', 'ryppka', 'pippka'];
+
+$data = file("data.txt", FILE_IGNORE_NEW_LINES);
 
 header("Content-type: application/json");
 echo json_encode($data);
